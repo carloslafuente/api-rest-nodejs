43385337eba605bd578ff792753fafeb1d0d9dbb
Carlos La Fuente <carlos.lafuente.dev@gmail.com>
2020-06-16 22:43:59 -0400

    Agregando los handlebars para el renderizado desde nodejs

--- app.js
+++ app.js
@@ -4,11 +4,28 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const hbs = require('express-handlebars');
 const app = express();
 const api = require('./routes/index');
 
-app.use(bodyParser.urlencoded({ extended: false, useNewUrlParser: true }));
-app.use(bodyParser.json({ useNewUrlParser: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.engine(
+	'.hbs',
+	hbs({
+		defaultLayout: 'default',
+		extname: '.hbs',
+	})
+);
+app.set('view engine', '.hbs');
+
 app.use('/api', api);
+app.get('/login', (req, res) => {
+	res.render('login');
+});
+app.get('/', (req, res) => {
+	res.render('product');
+});
 
 module.exports = app;
